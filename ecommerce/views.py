c3d73a0827c1193f65daa91f3b6c9a9f845834f0
Ukaegbu Gray Nneji <graynneji@outlook.com>
2021-08-13 22:47:48 +0200

    initial commit

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from store.models import Product
 
 def home(request):
-    return render(request, 'home.html')
+    #do the query to display your products in your html file home page
+    products = Product.objects.all().filter(is_available=True)
+
+    #we got all the products from this product variable
+    context = {
+        'products': products,
+        }
+    return render(request, 'home.html', context)
